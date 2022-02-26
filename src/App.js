70b9572cd29c127c83af42eae71f03d610642d8f
billfres <ngomafresnel@gmail.com>
2022-02-26 02:39:48 +0100

    Ajout de routes d'affichage employees

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -7,13 +9,19 @@ import FooterComponent from './components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-        <div className="container">
-          <ListEmployeeComponent />
-        </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+            <div className="container">
+              <Switch>
+                <Route path="/" component = {ListEmployeeComponent}></Route>
+                <Route path = '/employees' component = {ListEmployeeComponent}></Route>
+                <ListEmployeeComponent />
+              </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -15,7 +15,7 @@ class HeaderComponent  extends Component {
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                         <div>
-                            <a href="#" className="navbar-brand">Employee Management App</a>
+                            <a href="https:google.com" className="navbar-brand">Employee Management App</a>
                         </div>
                     </nav>
                 </header>
